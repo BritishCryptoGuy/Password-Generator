0b52564286b8582d61f7dd35ba24b938ab79652e
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2022-11-20 00:42:59 +0000

    Finished main script code, need to do some debugging

--- script/script.js
+++ script/script.js
@@ -1,33 +1,3 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  "@",
-  "%",
-  "+",
-  "\\",
-  "/",
-  "'",
-  "!",
-  "#",
-  "$",
-  "^",
-  "?",
-  ":",
-  ",",
-  ")",
-  "(",
-  "}",
-  "{",
-  "]",
-  "[",
-  "~",
-  "-",
-  "_",
-  ".",
-];
-
-// Array of numeric characters to be included in password
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
   "a",
@@ -88,9 +58,53 @@ var upperCasedCharacters = [
   "Z",
 ];
 
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Array of special characters to be included in password
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+//All options as amArray
+let allOptions = [
+  lowerCasedCharacters,
+  upperCasedCharacters,
+  numericCharacters,
+  specialCharacters,
+];
+
+//Startover write password function
 function startOver() {
   let carryOn = confirm("Would you like to start again?");
-  if (carryOn) writePassword();
+  if (carryOn) {
+    writePassword();
+  } else {
+    alert("No password this time!");
+  }
+  return;
 }
 
 // Function to prompt user for password options
@@ -107,14 +121,13 @@ function getPasswordOptions() {
     startOver();
     return;
   }
-  console.log(numChar);
   let lowerCase = confirm("Include lowercase letters?");
   let upperCase = confirm("Include uppercase letters?");
   let numericChar = confirm("Include numbers?");
   let specialChar = confirm("Include special characters?");
   let cases = [lowerCase, upperCase, numericChar, specialChar];
   if (cases.includes(true)) {
-    console.log("GG");
+    alert("Thanks for your input, your password will now be generated!");
   } else {
     alert("At least one character/number type must be selected.");
     startOver();
@@ -124,16 +137,37 @@ function getPasswordOptions() {
   return passChoices;
 }
 
+//randomIncludes function= pushes specified characters to own array
+function randomIncludes(cases) {
+  let allowedChars = [];
+  for (let i in cases) {
+    if (cases[i]) {
+      allowedChars.push(...allOptions[i]);
+    }
+  }
+  return allowedChars;
+}
+
 // Function for getting a random element from an array
-function getRandom(arr) {}
+function getRandom(arr) {
+  let numChar = arr.shift();
+  let cases = [...arr[0]];
+  let optionsArray = randomIncludes(cases);
+  return [numChar, optionsArray];
+}
 
 // Function to generate password with user input
 function generatePassword() {
   let passOptions = getPasswordOptions();
-  console.log(passOptions);
-  //will call get password options
-  //will call getRandom with password options
-  //must return password
+  let specs = [getRandom(passOptions)];
+  let passArray = [];
+  for (let num = 0; num <= specs[0][0]; num++) {
+    num;
+    passArray.push(specs[0][1][Math.floor(Math.random() * specs[0][1].length)]);
+  }
+  let password = passArray.join("");
+  console.log(password);
+  return password;
 }
 
 // Get references to the #generate element
@@ -143,8 +177,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  return;
 }
 
 // Add event listener to generate button
